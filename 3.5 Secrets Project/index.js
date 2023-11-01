79b40e45ff65bde19678f7ecaa8b9343ce3983b6
nirglus <nirgluskin@gmail.com>
2023-11-01 15:59:41 +0200

    add app.listen

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -11,3 +11,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port , () =>{
+    console.log(`Server is running on port ${port}`);
+})
