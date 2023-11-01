4761fc5174888a01028a5995f481d927af0341d1
nirglus <nirgluskin@gmail.com>
2023-11-01 15:58:19 +0200

    import packs

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,3 +1,13 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import dirname from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
