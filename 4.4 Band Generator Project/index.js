65f123a47c5ba32aab729ee6b2265aeebafcf8bb
nirglus <nirgluskin@gmail.com>
2023-11-06 13:00:11 +0200

    complete step 1

--- 4.4 Band Generator Project/index.js
+++ 4.4 Band Generator Project/index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {
