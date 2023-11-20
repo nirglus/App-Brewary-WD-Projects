5224a1ab746ed03cbd60e461ff069b4dbd6a8523
nirglus <nirgluskin@gmail.com>
2023-11-21 01:28:32 +0200

    complete 1, 2

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -1,7 +1,12 @@
 // HINTS:
 // 1. Import express and axios
+import express from "express";
+import axios from "axios";
 
 // 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
 
 // 3. Use the public folder for static files.
 
