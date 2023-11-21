ec55d169e57f4b875b2f3879e2f26d915a3730be
nirglus <nirgluskin@gmail.com>
2023-11-21 03:06:15 +0200

    add 4 + 5

--- 5.6 Secrets Project/index.js
+++ 5.6 Secrets Project/index.js
@@ -12,8 +12,16 @@ const API_URL = "https://secrets-api.appbrewery.com";
 app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
-app.get("/", (req, res) =>{
-    res.render("index.ejs");
+app.get("/", async (req, res) =>{
+    try {
+        const result = await axios.get(API_URL + "/random");
+        res.render("index.ejs", {
+            secret: result.data.secret,
+            user: result.data.username
+        });
+    } catch (error) {
+        res.render("index.ejs", {error: error.message});
+    }
 });
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
