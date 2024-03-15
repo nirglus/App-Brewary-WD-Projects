26eaa409b3dcaf6dac9e0b848924e27e977be13d
nirglus <nirgluskin@gmail.com>
2024-03-16 01:38:37 +0200

    display country codes from db in ejs

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -1,16 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import { pgConfig } from "./config/pgConfig";
+import { pgConfig } from "./config/pgConfig.js";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client(pgConfig);
+
+db.connect();
+let countries = [];
+db.query("SELECT country_code FROM visited_countries", (err, res) =>{
+  if(err){
+    console.error("Error executing query", err.stack);
+  } else {
+    countries = res.rows.map(row => row.country_code);
+  }
+  db.end();
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+   let total = countries.length;
+   console.log({countries});
+   res.render("index.ejs", {countries: countries, total: total})
 });
 
 app.listen(port, () => {
