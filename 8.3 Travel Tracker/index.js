de7163e53e8dfdf58a15fba6f6ac09e51b0b0968
nirglus <nirgluskin@gmail.com>
2024-03-17 20:59:19 +0200

    complete task 2

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -10,23 +10,45 @@ const db = new pg.Client(pgConfig);
 
 db.connect();
 let countries = [];
-db.query("SELECT country_code FROM visited_countries", (err, res) =>{
-  if(err){
-    console.error("Error executing query", err.stack);
-  } else {
-    countries = res.rows.map(row => row.country_code);
-  }
-  db.end();
-})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-   let total = countries.length;
-   console.log({countries});
-   res.render("index.ejs", {countries: countries, total: total})
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  countries = result.rows.map(row => row.country_code);
+  console.log({countries});
+  res.render("index.ejs", {countries: countries, total: countries.length})
+  db.end();
 });
+
+app.post("/add", async (req, res) =>{
+  let countryName = req.body.country.trim();
+  let countryCode = '';
+  countryName = countryName.charAt(0).toUpperCase() + countryName.slice(1).toLowerCase();
+  const findCountryQuery = `SELECT country_code FROM countries WHERE country_name = $1`;
+
+  db.query(findCountryQuery,[countryName], (err, result) =>{
+    if(err){
+      console.error("Failed to retrieve country code", err.stack);
+      return;
+    }
+    if(result.rows.length === 0){
+      console.error("Country not found");
+      res.status(404).send("Country not found");
+      return;
+    }
+      countryCode = result.rows[0].country_code;
+      db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode], (insertErr ,insertRes) =>{
+        if(insertErr){
+          console.error("Failed to insert country code", insertErr.stack);
+          return
+        }
+        res.redirect("/");
+      })
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
