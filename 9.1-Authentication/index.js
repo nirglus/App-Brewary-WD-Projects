1b249798780bea80407f246b8c195e1e46da9fcf
nirglus <nirgluskin@gmail.com>
2024-05-10 14:56:27 +0300

    complete level 1

--- 9.1-Authentication/index.js
+++ 9.1-Authentication/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import { pgConfig } from "./config/pgConfig.js";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client(pgConfig);
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -13,15 +18,59 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
+
 });
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      username,
+    ]);
 
-app.post("/login", async (req, res) => {});
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [username, password]
+      );
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      username,
+    ]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedPassword = user.password;
+
+      if (password === storedPassword) {
+        res.render("secrets.ejs");
+      } else {
+        res.send("Incorrect Password");
+      }
+    } else {
+      res.send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
